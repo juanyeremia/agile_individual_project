0878c9eb4fc294a0cfadb78767a47b2cfdb47e27
juanyeremia <juan.yovian@gmail.com>
2026-04-06 15:03:22 +0800

    Added interactive demo section. Added new JS for tutorial

---
+++ js/tutorial.js
@@ -0,0 +1,17 @@
+// Wait for page to load fully before accessing DOM elements
+document.addEventListener('DOMContentLoaded', function() {
+  // References to textarea, button, and output div
+  const input = $('#demo-input');
+  const runBtn = $('#run-btn');
+  const output = $('#demo-output');
+
+  // Listen for button click to run the code
+  runBtn.on('click', function() {
+
+    // Get current text in textarea
+    const userCode = input.val();
+
+    // Write into the output div as actual HTML
+    output.html(userCode);
+  });
+});
